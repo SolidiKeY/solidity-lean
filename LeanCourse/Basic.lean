9b7ddba07ad69fec97e6da376679af8c889fc4dd
alvares <alvares@chalmers.se>
2025-01-28 18:07:18 +0100

    fixed some errors

--- LeanCourse/Basic.lean
+++ LeanCourse/Basic.lean
@@ -4,27 +4,29 @@ open Sum
 
 inductive Struct (α β γ : Type) where
   | mtst : Struct α β γ
-  | var : Struct α β γ
+  | var (val : α) : Struct α β γ
   | store (st : Struct α β γ) (k : β ⊕ γ) (v : Struct α β γ) : Struct α β γ
 
 open Struct
 
-def save (st : Struct α β γ) (fields : List (β ⊕ γ)) (v : α ⊕ Struct α β γ) : Struct α β γ :=
+def save [DecidableEq β] [DecidableEq γ]
+  (st : Struct α β γ) (fields : List (β ⊕ γ)) (v : Struct α β γ) : Struct α β γ :=
   match st, fields with
-  | mtst, [] => sorry
+  | mtst, [] => v
   | mtst, k :: rest => store mtst k $ save mtst rest v
+  | var val, xs => sorry
   | store st k v, [] => store st k v
-  | store st k (inr v'), xs@(k' :: _) =>
-    if k = k' then store st k (inr v') else store (save st xs v) k (inr v')
-  | store st k (inl v'), xs@(k' :: rest) =>
-    if k = k' then store st k (inl $ save v' rest v) else store (save st xs v) k (inl v')
+  | store st k v', xs@(k' :: _) =>
+    if k = k' then store st k sorry else store (save st xs v) k sorry
 
-def select (st : Struct α β γ) (k : Nat) : Struct α β γ :=
+def select [DecidableEq β] [DecidableEq γ] (st : Struct α β γ) (k : β ⊕ γ) : Struct α β γ :=
   match st with
   | mtst => mtst
+  | var val => sorry
   | store st k' v => if k = k' then v else select st k'
 
-theorem selectSave (st : Struct α β γ) (k : Nat) (path : List Nat) (v : Struct α β γ) (k' : Nat) :
+theorem selectSave [DecidableEq β] [DecidableEq γ]
+  (st : Struct α β γ) (k : β ⊕ γ) (path : List (β ⊕ γ)) (v : Struct α β γ) (k' : β ⊕ γ) :
   select (save st (k :: path) v) k' =
   (if k = k' then save (select st k') path v else select st k') := by
   sorry
