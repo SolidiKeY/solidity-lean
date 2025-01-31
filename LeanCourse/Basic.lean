bcf99118497e1769bb6532339fa4c3ccec2a4f7d
alvares <alvares@chalmers.se>
2025-01-31 11:28:25 +0100

    fixed main components

--- LeanCourse/Basic.lean
+++ LeanCourse/Basic.lean
@@ -14,19 +14,23 @@ def save [DecidableEq β] [DecidableEq γ]
   match st, fields with
   | mtst, [] => v
   | mtst, k :: rest => store mtst k $ save mtst rest v
-  | var val, xs => sorry
-  | store st k v, [] => store st k v
-  | store st k v', xs@(k' :: _) =>
-    if k = k' then store st k sorry else store (save st xs v) k sorry
+  | var _, _ => v
+  | store st k _, [] => store st k v
+  | store st k v', xs@(k' :: ys) =>
+    if k = k' then store st k (save v' ys v) else store (save st xs v) k v'
 
 def select [DecidableEq β] [DecidableEq γ] (st : Struct α β γ) (k : β ⊕ γ) : Struct α β γ :=
   match st with
   | mtst => mtst
-  | var val => sorry
+  | var _ => mtst
   | store st k' v => if k = k' then v else select st k'
 
 theorem selectSave [DecidableEq β] [DecidableEq γ]
   (st : Struct α β γ) (k : β ⊕ γ) (path : List (β ⊕ γ)) (v : Struct α β γ) (k' : β ⊕ γ) :
   select (save st (k :: path) v) k' =
   (if k = k' then save (select st k') path v else select st k') := by
-  sorry
+  induction st
+  . sorry
+  . sorry
+  . sorry
+  done
